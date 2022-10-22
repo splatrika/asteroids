63df1aef420a9d269ae583810c9f77f3d10d3efb
daniilbelov <tseflutty@yandex.ru>
2022-10-22 19:00:52 +0300

    Add gameplay installer

---
+++ Assets/Source/Scene/Installers/GameplayInstaller.cs
@@ -0,0 +1,20 @@
+using Splatrika.Asteroids.Model;
+using UnityEngine;
+using Zenject;
+
+namespace Splatrika.Asteroids.Scene
+{
+    public class GameplayInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private CameraScreen _screen;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IScreen>()
+                .FromInstance(_screen);
+            Container.Bind<ILogger>()
+                .FromInstance(Debug.unityLogger);
+        }
+    }
+}
