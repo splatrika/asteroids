1e47e72a434c483c9e4f857f4cdfe8296ec06198
daniilbelov <tseflutty@yandex.ru>
2022-10-22 19:01:55 +0300

    Add common presenters

---
+++ Assets/Source/Presenter/Common/EntityDisposer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class EntityDisposer : MonoBehaviour
+{
+    private List<IDisposable> _models;
+
+
+    [Inject]
+    public void Init(List<IDisposable> models)
+    {
+        _models = models;
+    }
+
+
+    private void OnDestroy()
+    {
+        _models.ForEach(x => x.Dispose());
+    }
+}
---
+++ Assets/Source/Presenter/Common/EntityUpdater.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Splatrika.Asteroids.Model;
+using UnityEngine;
+using Zenject;
+
+public class EntityUpdater : MonoBehaviour
+{
+    private List<IUpdatable> _models;
+
+
+    [Inject]
+    public void Init(List<IUpdatable> models)
+    {
+        _models = models;
+    }
+
+
+    private void Update()
+    {
+        _models.ForEach(x => x.Update(Time.deltaTime));
+    }
+}
